2f36e611466364f8ccdc791183eb09cd9894efc4
Victoria-06 <ladyvic001@gmail.com>
2022-06-06 00:52:20 -0700

    my 3rd

--- php/reset.php
+++ php/reset.php
@@ -5,23 +5,20 @@ $password = $_POST['password'];
 
 //converting csv into multidimentional array
 $csv = array_map('str_getcsv', file('../storage/users.csv'));
-//column for name
-// $c_name = array_column($csv, 0);
-//column for email
+
 $c_email = array_column($csv, 1);
-//search the array row using the email in the email column, return the key
 $search = array_search($email, $c_email);
 //save the row where the user is present in the variable
 $row = ($csv[$search]);
 $row[2] = $password;
 //new user data in array data type
 $new = $row;
-// replace the array using the key(i.e $search)
+// replace the array using $search
 $csv[$search] = $new;
 
 // $new_csv = implode(",", $csv);
 
-$path = "../storage/users.csv"; //file path
+$path = "../storage/users.csv";
 $open = fopen($path, "w");
 foreach ($csv as $insert) {
     $confirm = fputcsv($open, $insert);
